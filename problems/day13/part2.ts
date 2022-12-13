65629ac2618a5516d354d2806d8ef0bf59d2b3dc
Michael Margel <michael.margel@shopify.com>
2022-12-13 03:14:55 -0500

    Day 13, Part 2, with numbers

--- problems/day13/part2.ts
+++ problems/day13/part2.ts
@@ -4,13 +4,30 @@ const findSolution = (values: string[]): number => {
   values.push("[[2]]");
   values.push("[[6]]");
 
-  const stringify = (str: string) =>
-    str
-      .replace(/\[\]/g, "-1")
+  const stringify = (str: string) => {
+    let numericString = str.replace(/\[\]/g, "A");
+    let currentNumber = 0;
+    // Ideally, we'd repeat until we miss.
+    while (currentNumber < 20) {
+      numericString = numericString.replace(
+        new RegExp(`\\[${String.fromCharCode(65 + currentNumber)}\\]`, "g"),
+        `${String.fromCharCode(65 + currentNumber + 1)}`
+      );
+      currentNumber++;
+    }
+
+    return numericString
       .replace(/[\[\]]/g, "")
       .split(",")
-      .map((num) => String.fromCharCode(+num + 65))
+      .map((char) => {
+        if (char.match(/^[A-Z]+$/)) {
+          return String.fromCharCode(char.charCodeAt(0) - 65 + 48);
+        } else {
+          return String.fromCharCode(+char + 65);
+        }
+      })
       .join("");
+  };
 
   const strings = values
     .filter((row) => row != "")
